1273e9041f5fb4087eef42c5b01e20d6ae99df18
JFredMC <maquiloncordoba@hotmail.com>
2022-03-28 08:06:16 -0500

    Editor de texto básico creado con Node.js

---
+++ Editor_texto/messages.js
@@ -0,0 +1,10 @@
+const messages = {
+    fileSaved : 'Archivo guardado con exito, puedes seguir editando...',
+    fileNotSaved : 'Archivo no guardado, puedes seguir editando...',
+    requestFileName : 'Nombre del archivo: ',
+    replaceFile : 'Deseas sustituir el archivo? (S/N)',
+    fileNotFound : 'El archivo no existe!',
+    fileExists : 'Ya existe el archivo'
+};
+
+module.exports = messages;
